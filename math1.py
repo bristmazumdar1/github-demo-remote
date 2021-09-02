c6a0fbd2b6066186a8bd523eaa3a10866b29ed5e
bristimazumdar <bristi_mazumdar@persistent.com>
2021-09-02 23:13:07 +0530

    added fn headers

--- math1.py
+++ math1.py
@@ -1,12 +1,13 @@
+#add implementation
 def add(x,y):
     pass
-
+#subtract implementation
 def subtract(x,y):
     pass             
-
+#multiply implementation
 def multiply(x,y):
     pass
-
+#divide implementation
 def divide(x,y):
     pass
 
